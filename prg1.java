8ba4145898c6bf7319577b5433dea3ca0c0fa15f
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2021-09-04 15:54:30 +0530

    prg1.java

---
+++ prg1.java
@@ -0,0 +1,9 @@
+public class prg1 {
+    public static void main(String args[]) {
+      int x1=30;
+      int y1=25;
+      int z1=x1-y1;
+
+      System.out.println("Sum of x1+y1 = " + z1);
+    }
+}
